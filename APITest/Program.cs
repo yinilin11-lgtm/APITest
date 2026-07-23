268193118dd0fffef40e713124ff968a1a2bc102
Iris Lin <yinilin11@gmail.com>
2026-07-23 15:58:30 +0800

    Add Swagger UI

--- APITest/Program.cs
+++ APITest/Program.cs
@@ -10,6 +10,8 @@ namespace APITest
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
@@ -19,6 +21,8 @@ namespace APITest
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
